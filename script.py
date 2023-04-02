cb1b1be8fdd50c09896fc79078e7a08a90f9b6bf
Jeff Z <jeffzh893@gmail.com>
2023-04-02 01:34:58 -0500

    Remove certain code

--- script.py
+++ script.py
@@ -1,20 +1,4 @@
-import math
-import os
-import sys
-
 import requests
-
-
-def is_venv():
-    return hasattr(sys, "real_prefix") or (
-        hasattr(sys, "base_prefix") and sys.base_prefix != sys.prefix
-    )
-
-
-if is_venv():
-    print("inside virtualenv or venv")
-else:
-    print("outside virtualenv or venv")
 
 
 name = input("Your name? ")
